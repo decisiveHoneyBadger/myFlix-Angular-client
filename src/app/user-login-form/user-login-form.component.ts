dd4054138a05c079f068c17dcf0125f4c036ebad
decisiveHoneyBadger <k.guevenc@web.de>
2022-08-15 17:29:20 +0200

    added user registration and login forms

---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '' };
+  
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+
+
+  /**
+   * sends login form inputs to the backend via fetchApiData 
+   */
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+      this.dialogRef.close(); // Close the modal on success
+      console.log(response);
+      // Add token and username to local Storage
+      localStorage.setItem('token', response.token);
+      localStorage.setItem('user', response.user.Username);
+
+      // Redirect to movies (main) page
+      this.router.navigate(['movies']);
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+}
