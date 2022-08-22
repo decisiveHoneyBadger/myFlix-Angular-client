bcbe2efd537d22bcb5936cb0b5d5a93d959602b1
decisiveHoneyBadger <k.guevenc@web.de>
2022-08-22 15:56:05 +0200

    fixed syntax errors

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -3,11 +3,10 @@ import { Component, OnInit, Input } from '@angular/core';
 // You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
 
-// This import brings in the API calls we created in 6.2
-import { UserRegistrationService } from '../fetch-api-data.service';
-
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
+
+// imports the API calls created in 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
